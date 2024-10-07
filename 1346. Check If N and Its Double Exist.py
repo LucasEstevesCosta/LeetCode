ee69632f912651c3bd7ba1607e34834cdf72e375
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2024-10-07 17:24:52 -0300

    Update 1346. Check If N and Its Double Exist.py

--- 1346. Check If N and Its Double Exist.py
+++ 1346. Check If N and Its Double Exist.py
@@ -1,4 +1,21 @@
 def checkIfExist(arr: list[int]) -> bool:
+    """
+    Given a list of integers arr, determine whether there exists a pair of numbers
+    where one number is double the other.
+
+    Args:
+        arr: A list of integers.
+
+    Returns:
+        True if there exists a pair of numbers where one number is double the other, 
+        False otherwise.
+    """
+    seen = set()
+    for num in arr:
+        if num * 2 in seen or (num % 2 == 0 and num / 2 in seen):
+            return True
+        seen.add(num)
+    return False
 
 
 """TEST CASES"""
@@ -8,5 +25,13 @@ example1 = [10,2,5,3] # Output: true
 example2 = [3,1,7,11] # Output: false
 """Explanation: There is no i and j that satisfy the conditions."""
 
-print(checkIfExist(example1))
-print(checkIfExist(example2))
+example3 = [7,1,14,11] # Output: true
+"""Explanation: There is no i and j that satisfy the conditions."""
+
+example4 = [0,0] # Output: true
+"""Explanation: There is no i and j that satisfy the conditions."""
+
+print(f'{checkIfExist(example1)} OUTPUT: True')
+print(f'{checkIfExist(example2)} OUTPUT: False')
+print(f'{checkIfExist(example3)} OUTPUT: True')
+print(f'{checkIfExist(example4)} OUTPUT: True')
