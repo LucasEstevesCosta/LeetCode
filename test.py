b3ec62e0820c6d7c6ec17566037650b553fd0b33
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2024-10-11 14:29:50 -0300

    exe 905. Sort Array by Parity
    Added new algorithm to solution in comments and test file.

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-def remove_element(nums: list[int], val: int) -> int:
-    k = 0
-    for num in nums:
-        if num != val:
-            nums[k] = num
-            k += 1
-    return k
-
-    """Versão alternativa
+"""ARQUIVO QUE USO PARA TESTAR ALGUMAS COISAS"""
+def removeElement(nums: list[int], val: int) -> int:
     pi = 0
     pf = len(nums) - 1
     while pi <= pf:
@@ -20,25 +13,24 @@ def remove_element(nums: list[int], val: int) -> int:
         else:
             pi += 1
         
-    return pi"""
-
-
+    return pi
+        
 """TEST CASES"""
 example1 = [3, 2, 2, 3]
 val1 = 3
-"""Output: 2, nums = [2,2,_,_]
+"""Output: 2, nums = [2, 2, _, _]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores)."""
 
 example2 = [0, 1, 2, 2, 3, 0, 4, 2]
 val2 = 2
-"""Output: 5, nums = [0,1,4,0,3,_,_,_]
+"""Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
 Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 Note that the five elements can be returned in any order.
 It does not matter what you leave beyond the returned k (hence they are underscores)."""
 
-k1 = remove_element(example1, val1)
-print(f"k1 = {k1}, example1 = {example1[:k1]}")
+k1 = removeElement(example1, val1)
+print(f"k1 = {k1}, example1 = {example1[:k1]}; EXPECTED OUTPUT: [2, 2]")
 
-k2 = remove_element(example2, val2)
-print(f"k2 = {k2}, example2 = {example2[:k2]}")
+k2 = removeElement(example2, val2)
+print(f"k2 = {k2}, example2 = {example2[:k2]}; EXPECTED OUTPUT: [0, 1, 4, 0, 3]")
