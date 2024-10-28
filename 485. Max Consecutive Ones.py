711ef7e4a4aa1513b2dd2a49ef302f822c29ed5d
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2024-10-28 13:03:08 -0300

    Training max consecutive ones.

--- 485. Max Consecutive Ones.py
+++ 485. Max Consecutive Ones.py
@@ -3,12 +3,12 @@ import unittest
 
 
 def find_max_consecutive_ones(nums: list[int]) -> int:
-    temp_counter = 0
     final_counter = 0
-    for index, num in enumerate(nums):
+    temp_counter = 0
+    for pos, num in enumerate(nums):
         if num == 1:
             temp_counter += 1
-        if num == 0 or index == len(nums) - 1:
+        if num == 0 or pos == len(nums) - 1:
             if temp_counter > final_counter:
                 final_counter = temp_counter
             temp_counter = 0
@@ -16,8 +16,6 @@ def find_max_consecutive_ones(nums: list[int]) -> int:
 
 
 """TESTS"""
-
-
 class Test_find_max_consecutive_ones(unittest.TestCase):
     def test_ex1(self):
         self.assertEqual(find_max_consecutive_ones([1, 1, 0, 1, 1, 1]), 3)
