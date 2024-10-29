b5a513dff4fdcb324741974789ffcabfefe02fba
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2024-10-29 09:08:56 -0300

    Update 1295. Find Numbers with Even Number of Digits.py
    Training in this exercise.

--- 1295. Find Numbers with Even Number of Digits.py
+++ 1295. Find Numbers with Even Number of Digits.py
@@ -1,31 +1,31 @@
 """Given an array nums of integers, return how many of them contain an even number of digits."""
 
 
+import unittest
+
+
 def find_numbers(nums: list[int]) -> int:
     """
     :type nums: List[int]
     :rtype: int
     """
-    tot_even = 0
-    for i in nums:
-        if len(str(i)) % 2 == 0:
-            tot_even += 1
-    return tot_even
+    tot_even_digit_numbers = 0
+    for num in nums:
+        if len(str(num)) % 2 == 0:
+            tot_even_digit_numbers += 1
+    return tot_even_digit_numbers
 
 
-"""Case Tests"""
-example1 = [12,345,2,6,7896]  # Output: 2
-"""Explanation:
-12 contains 2 digits (even number of digits). 
-345 contains 3 digits (odd number of digits). 
-2 contains 1 digit (odd number of digits). 
-6 contains 1 digit (odd number of digits). 
-7896 contains 4 digits (even number of digits). 
-Therefore only 12 and 7896 contain an even number of digits."""
+# TEST CASES
 
-example2 = [555,901,482,1771]  # Output: 1
-"""Explanation: 
-Only 1771 contains an even number of digits."""
 
-print(find_numbers(example1))
-print(find_numbers(example2))
+class TestFindNumbers(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(find_numbers([12, 345, 2, 6, 7896]), 2)
+
+    def test_ex2(self):
+        self.assertEqual(find_numbers([555, 901, 482, 1771]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
